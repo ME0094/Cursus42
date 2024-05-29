c3446f76324103dbdbe96a8b4457ea6a20308604
Martín Eliseo <martineliseo.info@gmail.com>
2024-05-29 19:25:55 +0200

    Updated ft_putstr

--- ft_printf/ft_putstr.c
+++ ft_printf/ft_putstr.c
@@ -6,20 +6,29 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:20:55 by martirod          #+#    #+#             */
-/*   Updated: 2024/05/27 20:04:36 by martirod         ###   ########.fr       */
+/*   Updated: 2024/05/29 19:24:59 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putstr(char *str)
+int	ft_putstr(char *str)
 {
-	int	i;
+	int	len;
 
-	i = 0;
-	while (str[i])
+	if (!str)
 	{
-		write(1, &str[i], i);
-		i++;
+		if (write (1, "(null)", 6) == -1)
+			return (-1);
+		return (6);
 	}
+	len = 0;
+	while (*str != '\0')
+	{
+		if (write(1, str, 1) == -1)
+			return (-1);
+		str++;
+		len++;
+	}
+	return (len);
 }
