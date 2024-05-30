0510374d7bfb659ace6dae867908cbbc55037a30
Martin <martineliseo.info@gmail.com>
2024-05-30 13:50:11 +0200

    Rewrite of function

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -6,31 +6,36 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:21:14 by martirod          #+#    #+#             */
-/*   Updated: 2024/05/29 20:29:41 by martirod         ###   ########.fr       */
+/*   Updated: 2024/05/30 13:49:53 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putnbr(int n)
+int	ft_putnbr(long nb)
 {
-	if (n == -2147483648)
+	long		num;
+	int			len;
+	int			aux;
+
+	len = 0;
+	if (nb < 0)
 	{
-		ft_putstr("-2147483648");
-		return ;
+		if (write(1, "-", 1) == -1)
+			return (-1);
+		len += 1;
+		nb *= -1;
 	}
-	if (n < 0)
+	num = nb % 10 + '0';
+	if (nb > 9)
 	{
-		ft_putchar('-');
-		n = -n;
+		aux = ft_putnbr(nb / 10);
+		if (aux == -1)
+			return (-1);
+		len += aux;
 	}
-	if (n >= 10)
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-	else
-	{
-		ft_putchar(n + '0');
-	}
+	if (write(1, &num, 1) == -1)
+		return (-1);
+	len += 1;
+	return (len);
 }
