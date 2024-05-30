b7d8f64e532ac0fd233b50990177935e8e28d88f
Martin <martineliseo.info@gmail.com>
2024-05-30 13:02:21 +0200

    Changed return

--- ft_printf/ft_putchar.c
+++ ft_printf/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:21:32 by martirod          #+#    #+#             */
-/*   Updated: 2024/05/29 20:29:53 by martirod         ###   ########.fr       */
+/*   Updated: 2024/05/30 13:01:44 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int	ft_putchar(char c)
 {
-	if (write(1, &c, 1) != 1)
-		return (-1);
-	return (1);
+	return (write(1, &c, 1));
 }
