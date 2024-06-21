04774ce378914ccaeb09d790f438e476244fb0b3
Martín Eliseo <martineliseo.info@gmail.com>
2024-06-21 19:17:08 +0200

    Created .h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,21 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 15:22:30 by martirod          #+#    #+#             */
-/*   Updated: 2024/06/20 15:22:33 by martirod         ###   ########.fr       */
+/*   Updated: 2024/06/21 19:15:47 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
