d1ce520efbf9dc950cdd77ae4af190664e1a20a3
Martín Eliseo <martineliseo.info@gmail.com>
2024-07-31 18:33:35 +0200

    Updated push_swap.h

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 17:24:27 by martirod          #+#    #+#             */
-/*   Updated: 2024/07/31 18:00:25 by martirod         ###   ########.fr       */
+/*   Updated: 2024/07/31 18:32:10 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,35 @@ typedef struct s_stack
 
 /* Stack */
 
-void	ft_stack_add_back(t_stack **stack, t_stack *new_node);
-t_stack	*ft_stack_new(int data, int position);
-int		ft_stack_size(t_stack *head);
+void			ft_stack_add_back(t_stack **stack, t_stack *new_node);
+t_stack			*ft_stack_new(int data, int position);
+int				ft_stack_size(t_stack *head);
 
 /* Initialization */
 
-int		*ft_create_array_from_stack(t_stack *stack_a);
-int		ft_init_stack(t_stack **stack, int argc, char **argv);
+int				*ft_create_array_from_stack(t_stack *stack_a);
+int				ft_init_stack(t_stack **stack, int argc, char **argv);
 
 /* Validation */
 
+long long int	ft_atoll(char *nbr, t_stack **a, char **t);
+int				ft_validate_arg(char **tokens);
+int				ft_has_doubles(t_stack **stack_a);
+int				ft_is_sorted(t_stack *stack_a);
+
 /* Movements */
 
-void	ft_sa(t_stack **stack_a);
-void	ft_sb(t_stack **stack_b);
-void	ft_ss(t_stack **stack_a, t_stack **stack_b);
-void	ft_pa(t_stack **stack_a, t_stack **stack_b);
-void	ft_pb(t_stack **stack_a, t_stack **stack_b);
-void	ft_ra(t_stack **stack_a);
-void	ft_rb(t_stack **stack_b);
-void	ft_rr(t_stack **stack_a, t_stack **stack_b);
-void	ft_rra(t_stack **stack_a);
-void	ft_rrb(t_stack **stack_b);
-void	ft_rrr(t_stack **stack_a, t_stack **stack_b);
+void			ft_sa(t_stack **stack_a);
+void			ft_sb(t_stack **stack_b);
+void			ft_ss(t_stack **stack_a, t_stack **stack_b);
+void			ft_pa(t_stack **stack_a, t_stack **stack_b);
+void			ft_pb(t_stack **stack_a, t_stack **stack_b);
+void			ft_ra(t_stack **stack_a);
+void			ft_rb(t_stack **stack_b);
+void			ft_rr(t_stack **stack_a, t_stack **stack_b);
+void			ft_rra(t_stack **stack_a);
+void			ft_rrb(t_stack **stack_b);
+void			ft_rrr(t_stack **stack_a, t_stack **stack_b);
 
 /* Sorting */
 
