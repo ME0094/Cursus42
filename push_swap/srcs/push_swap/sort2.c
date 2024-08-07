9b91d880a3aa17d4ebd118834f158574a34edd70
Martín Eliseo <martineliseo.info@gmail.com>
2024-08-07 19:28:43 +0200

    Added comments

--- push_swap/srcs/push_swap/sort2.c
+++ push_swap/srcs/push_swap/sort2.c
@@ -6,9 +6,75 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 18:40:35 by martirod          #+#    #+#             */
-/*   Updated: 2024/08/07 18:26:46 by martirod         ###   ########.fr       */
+/*   Updated: 2024/08/07 19:27:51 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/**
+ * Pushes back elements from stack B to stack A.
+ */
+void	pushing_back_to_a(t_stack **stack_a, t_stack **stack_b, int size)
+{
+	int	median;
+	int	location;
+
+	median = size / 2;
+	while (*stack_b)
+	{
+		location = rb_or_rrb(*stack_b, size);
+		if (location <= median)
+			ft_rb(*stack_b);
+		else
+			ft_pa(*stack_a, stack_b);
+		size--;
+	}
+}
+
+/**
+ * Sorts the stack.
+ */
+void	sorting_to_hundred(t_stack **stack_a, t_stack **stack_b, int size)
+{
+	while (*stack_a)
+	{
+		if ((*stack_a)->index == 0)
+		{
+			ft_pb(stack_a, stack_b);
+			ft_rb(stack_b);
+		}
+		else
+			ft_pb(stack_a, stack_b);
+	}
+	pushing_back_to_a(stack_a, stack_b, size);
+}
+
+/**
+ * Sorts two numbers in ascending order.
+ */
+void	sorting_two_numbers(t_stack **stack_a)
+{
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	if ((*stack_a)->value > (*stack_a)->next->value)
+		ft_sa(stack_a);
+}
+
+/**
+ * Sorts the stacks using a specific algorithm.
+ */
+void	sorting(t_stack **stack_a, t_stack **stack_b, int size)
+{
+	while (*stack_a)
+	{
+		if ((*stack_a)->index == 0)
+		{
+			ft_pb(stack_a, stack_b);
+			ft_rb(stack_b);
+		}
+		else
+			ft_pb(stack_a, stack_b);
+	}
+	pushing_back_to_a(stack_a, stack_b, size);
+}
