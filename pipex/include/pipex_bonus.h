bd38c88692057f3aa4803073629c1831c77d8ed0
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-09 17:30:31 +0200

    Libraries added

--- pipex/include/pipex_bonus.h
+++ pipex/include/pipex_bonus.h
@@ -6,13 +6,18 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:19:50 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/09 15:22:24 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:30:19 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_BONUS_H
 # define PIPEX_BONUS_H
 
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include "../libft/libft.h"
 # include "../libft/ft_printf/ft_printf.h"
 
