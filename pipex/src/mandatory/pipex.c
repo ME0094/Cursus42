83aebcd02583446562ae318cc365816374e704bf
Martin Eliseo <martineliseo.info@gmail.com>
2024-10-13 20:00:11 +0200

    Refactorized pipex.c

--- pipex/src/mandatory/pipex.c
+++ pipex/src/mandatory/pipex.c
@@ -6,40 +6,31 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:15:20 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/13 18:56:55 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/13 19:59:32 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
-#include "pipex.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
-
 /* Function to handle the first child process */
-static void	handle_first_child(int *pipe_fd, char **argv, char **envp)
+static int	open_input_file(char *file)
 {
-	int		input_fd;
-	char	**cmd_args;
-	char	*cmd_path;
-
-	close(pipe_fd[0]);
-	input_fd = get_file_descriptor(argv[1], 0);
+	int input_fd = get_file_descriptor(file, 0);
 	if (input_fd < 0)
 	{
 		perror("Error opening input file");
 		exit(EXIT_FAILURE);
 	}
-	dup2(input_fd, STDIN_FILENO);
-	close(input_fd);
-	dup2(pipe_fd[1], STDOUT_FILENO);
-	close(pipe_fd[1]);
-	cmd_args = ft_split(argv[2], ' ');
-	cmd_path = get_command_path(cmd_args, envp);
+	return input_fd;
+}
+
+static void	execute_command(char *cmd, char **envp)
+{
+	printf("Debug: Entering execute_command\n");
+	char **cmd_args = ft_split(cmd, ' ');
+	printf("Debug: Command arguments split\n");
+	char *cmd_path = get_command_path(cmd_args, envp);
+	printf("Debug: Command path obtained\n");
 	if (execve(cmd_path, cmd_args, envp) == -1)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
@@ -50,63 +41,78 @@ static void	handle_first_child(int *pipe_fd, char **argv, char **envp)
 	}
 }
 
-/* Function to handle the second child process */
-static void	handle_second_child(int *pipe_fd, char **argv, char **envp)
+static void	handle_first_child(int *pipe_fd, char **argv, char **envp)
 {
-	int		output_fd;
-	char	**cmd_args;
-	char	*cmd_path;
-
+	printf("Debug: Entering handle_first_child\n");
+	int input_fd = open_input_file(argv[1]);
+	printf("Debug: Input file opened\n");
+	close(pipe_fd[0]);
+	dup2(input_fd, STDIN_FILENO);
+	close(input_fd);
+	dup2(pipe_fd[1], STDOUT_FILENO);
 	close(pipe_fd[1]);
-	output_fd = get_file_descriptor(argv[4], 1);
+	execute_command(argv[2], envp);
+}
+
+/* Function to handle the second child process */
+static int	open_output_file(char *file)
+{
+	int output_fd = get_file_descriptor(file, 1);
 	if (output_fd < 0)
 	{
 		perror("Error opening output file");
 		exit(EXIT_FAILURE);
 	}
+	return output_fd;
+}
+
+static void	handle_second_child(int *pipe_fd, char **argv, char **envp)
+{
+	printf("Debug: Entering handle_second_child\n");
+	int output_fd = open_output_file(argv[4]);
+	printf("Debug: Output file opened\n");
+	close(pipe_fd[1]);
 	dup2(pipe_fd[0], STDIN_FILENO);
 	close(pipe_fd[0]);
 	dup2(output_fd, STDOUT_FILENO);
 	close(output_fd);
-	cmd_args = ft_split(argv[3], ' ');
-	cmd_path = get_command_path(cmd_args, envp);
-	if (execve(cmd_path, cmd_args, envp) == -1)
-	{
-		ft_putstr_fd("pipex: command not found: ", 2);
-		ft_putendl_fd(cmd_args[0], 2);
-		free_string_array(cmd_args);
-		free(cmd_path);
-		exit(EXIT_FAILURE);
-	}
+	execute_command(argv[3], envp);
 }
 
 /* Function to handle the piping and forking */
-static void	handle_piping_and_forking(char **argv, char **envp)
+static void	create_pipe(int *pipe_fd)
 {
-	pid_t	pid;
-	int		pipe_fd[2];
-
 	if (pipe(pipe_fd) == -1)
 	{
 		perror("Error creating pipe");
 		exit(EXIT_FAILURE);
 	}
-	pid = fork();
+}
+
+static pid_t	fork_process()
+{
+	pid_t pid = fork();
 	if (pid < 0)
 	{
 		perror("Error forking process");
 		exit(EXIT_FAILURE);
 	}
+	return pid;
+}
+
+static void	handle_piping_and_forking(char **argv, char **envp)
+{
+	printf("Debug: Entering handle_piping_and_forking\n");
+	int pipe_fd[2];
+	create_pipe(pipe_fd);
+	printf("Debug: Pipe created\n");
+	pid_t pid = fork_process();
+	printf("Debug: First fork done\n");
 	if (pid == 0)
 		handle_first_child(pipe_fd, argv, envp);
 	else
 	{
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("Error forking process");
-			exit(EXIT_FAILURE);
-		}
+		pid = fork_process();
 		if (pid == 0)
 			handle_second_child(pipe_fd, argv, envp);
 		else
