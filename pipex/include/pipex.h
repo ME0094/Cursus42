bc8852a496df0339704ba3642423ad89b8627c05
Martin Eliseo <martineliseo.info@gmail.com>
2024-10-13 22:51:14 +0200

    "Updated pipex.h: modified comment with new timestamp, added 3 new function declarations, and rearranged function order."

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:45:04 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/13 22:23:59 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/13 22:50:54 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,12 @@ extern void execute_second_child(int *pipe_fd, char **argv, char **envp);
 extern void setup_input(int *pipe_fd, char *file);
 extern void setup_output(int *pipe_fd);
 extern void setup_output_file(int *pipe_fd, char *file);
-extern void execute_command(char *cmd, char **envp);
+
 extern void handle_file_descriptors(int argc, char **argv);
 void create_pipe(int *pipe_fd);
+int open_input_file(char *file);
+int open_output_file(char *file);
+void handle_piping_and_forking(char **argv, char **envp);
 pid_t fork_process();
 
 /* Utility functions */
