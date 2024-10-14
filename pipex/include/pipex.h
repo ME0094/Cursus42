5b96149429553e776d90922ba3c515f1213791d6
Martin Eliseo <martineliseo.info@gmail.com>
2024-10-14 17:42:39 +0200

    "Updated pipex.h and pipex.c files: added new function declarations, updated timestamps"

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:45:04 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 17:14:40 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:42:32 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # include "../libft/ft_printf/ft_printf.h"
 
 void child_process(int *pipefd, char **argv, char **envp);
+void child_process_logic_1(int *fd, char **argv, char **envp);
+void child_process_logic_2(int *fd, char **argv, char **envp);
 void dup_and_close(int oldfd, int newfd);
 void handle_error(const char *msg);
 void handle_pipe_error(void);
@@ -28,6 +30,7 @@ int open_file(char *file, int flags, const char *error_msg);
 int ft_get_fd(char *file, int mode);
 int main(int argc, char **argv, char **envp);
 void pipex(int argc, char **argv, char **envp);
+void ft_pipex(char **argv, char **envp);
 void check_command(char *arg, char **envp);
 void ft_check_cmd(int argc, char **argv, char **envp, int i);
 char *ft_get_path(char **argv_sp, char **envp);
