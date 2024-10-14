3ce60573c7b38bb08e43c2efedfe075449b18469
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 19:40:04 +0200

    "Formatted code in util_command.c: added whitespace and indentation to improve readability."

--- pipex/src/mandatory/util_command.c
+++ pipex/src/mandatory/util_command.c
@@ -6,31 +6,39 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:20:39 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 17:53:48 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:39:56 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
-void check_command(char *arg, char **envp) {
-    char *path;
-    char **argv_sp = ft_split(arg, ' ');
-    path = ft_get_path(argv_sp, envp);
-    if (!path) {
-        perror("Error finding command path");
-        ft_putstr_fd("invalid CMD: ", 2);
-        ft_putendl_fd(argv_sp[0], 2);
-        ft_free(argv_sp);
-        exit(EXIT_FAILURE);
-    } else {
-        ft_free(argv_sp);
-        free(path);
-    }
+void	check_command(char *arg, char **envp)
+{
+	char	*path;
+	char	**argv_sp;
+
+	argv_sp = ft_split(arg, ' ');
+	path = ft_get_path(argv_sp, envp);
+	if (!path)
+	{
+		perror("Error finding command path");
+		ft_putstr_fd("invalid CMD: ", 2);
+		ft_putendl_fd(argv_sp[0], 2);
+		ft_free(argv_sp);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		ft_free(argv_sp);
+		free(path);
+	}
 }
 
-void ft_check_cmd(int argc, char **argv, char **envp, int i) {
-    while (i <= (argc - 2)) {
-        check_command(argv[i], envp);
-        i++;
-    }
+void	ft_check_cmd(int argc, char **argv, char **envp, int i)
+{
+	while (i <= (argc - 2))
+	{
+		check_command(argv[i], envp);
+		i++;
+	}
 }
