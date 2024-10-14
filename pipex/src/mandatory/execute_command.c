26b9c69a5f22f091ee6b88652854204ab1a2f1f0
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 19:25:10 +0200

    "Formatted execute_command function with consistent indentation and whitespace."

--- pipex/src/mandatory/execute_command.c
+++ pipex/src/mandatory/execute_command.c
@@ -6,17 +6,19 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:16:42 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 17:53:12 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:24:57 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
-void execute_command(char *path, char **argv_sp, char **envp) {
-    if (execve(path, argv_sp, envp) == -1) {
-        perror("Error executing command");
-        ft_free(argv_sp);
-        free(path);
-        exit(EXIT_FAILURE);
-    }
+void	execute_command(char *path, char **argv_sp, char **envp)
+{
+	if (execve(path, argv_sp, envp) == -1)
+	{
+		perror("Error executing command");
+		ft_free(argv_sp);
+		free(path);
+		exit(EXIT_FAILURE);
+	}
 }
