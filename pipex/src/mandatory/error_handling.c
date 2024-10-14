448d771cb41e8322e06e08782ff95bccb8dd68b7
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 19:24:26 +0200

    "Formatted error handling functions in mandatory/error_handling.c"

--- pipex/src/mandatory/error_handling.c
+++ pipex/src/mandatory/error_handling.c
@@ -6,23 +6,26 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:16:10 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 17:53:00 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:24:11 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
-void handle_error(const char *msg) {
-    perror(msg);
-    exit(EXIT_FAILURE);
+void	handle_error(const char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
 }
 
-void handle_pipe_error(void) {
-    perror("Error creating pipe");
-    exit(EXIT_FAILURE);
+void	handle_pipe_error(void)
+{
+	perror("Error creating pipe");
+	exit(EXIT_FAILURE);
 }
 
-void handle_fork_error(void) {
-    perror("Error forking process");
-    exit(EXIT_FAILURE);
+void	handle_fork_error(void)
+{
+	perror("Error forking process");
+	exit(EXIT_FAILURE);
 }
