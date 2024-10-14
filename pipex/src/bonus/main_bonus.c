2465ee13088e4c3fb0ef451990932b91d54a6f34
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 18:34:40 +0200

    "Updated main_bonus.c: reformatted code, added whitespace, and updated timestamp in comment"

--- pipex/src/bonus/main_bonus.c
+++ pipex/src/bonus/main_bonus.c
@@ -6,18 +6,20 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:14:08 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 15:30:20 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:34:22 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
-#include <unistd.h>
 
-int main(int argc, char **argv, char **envp) {
-    validate_arguments(argc);
-    int i = ft_check_input(argv);
-    ft_check_cmd(argc, argv, envp, i);
-    ft_dups(argc, argv);
-    handle_fork_and_process(argc, argv, envp, i);
-    return 0;
+int	main(int argc, char **argv, char **envp)
+{
+	int	i;
+
+	validate_arguments(argc);
+	i = ft_check_input(argv);
+	ft_check_cmd(argc, argv, envp, i);
+	ft_dups(argc, argv);
+	handle_fork_and_process(argc, argv, envp, i);
+	return (0);
 }
