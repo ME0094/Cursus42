dbecd25830ebde0cf60ef334f632997feb947b99
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 18:30:12 +0200

    "Updated child_bonus.c: reformatted code, updated timestamp in comment"

--- pipex/src/bonus/child_bonus.c
+++ pipex/src/bonus/child_bonus.c
@@ -6,7 +6,7 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:11:57 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 15:30:13 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:29:55 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static void ft_child(char *cmd, char **envp) {
-    char **cmd_sp = ft_split(cmd, ' ');
-    char *path = ft_get_path(cmd_sp, envp);
-    if (execve(path, cmd_sp, envp) == -1) {
-        perror("Error executing command");
-        ft_free(cmd_sp);
-        free(path);
-        exit(EXIT_FAILURE);
-    }
+void	ft_child(char *cmd, char **envp)
+{
+	char	**cmd_sp;
+	char	*path;
+
+	cmd_sp = ft_split(cmd, ' ');
+	path = ft_get_path(cmd_sp, envp);
+	if (execve(path, cmd_sp, envp) == -1)
+	{
+		perror("Error executing command");
+		ft_free(cmd_sp);
+		free(path);
+		exit(EXIT_FAILURE);
+	}
 }
 
-void child_process_logic(int *fd, char *cmd, char **envp) {
-    close(fd[0]);
-    if (dup2(fd[1], STDOUT_FILENO) < 0) {
-        perror("Error duplicating file descriptor for pipe");
-        exit(EXIT_FAILURE);
-    }
-    close(fd[1]);
-    ft_child(cmd, envp);
+void	child_process_logic(int *fd, char *cmd, char **envp)
+{
+	close(fd[0]);
+	if (dup2(fd[1], STDOUT_FILENO) < 0)
+	{
+		perror("Error duplicating file descriptor for pipe");
+		exit(EXIT_FAILURE);
+	}
+	close(fd[1]);
+	ft_child(cmd, envp);
 }
