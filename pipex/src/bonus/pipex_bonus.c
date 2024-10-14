5bd6dc712bf3690617cad7b280486cc5d0041033
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 18:37:21 +0200

    "Updated ft_pipex function signature and implementation"

--- pipex/src/bonus/pipex_bonus.c
+++ pipex/src/bonus/pipex_bonus.c
@@ -6,34 +6,33 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:14:53 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 15:30:25 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:37:12 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-void ft_pipex(char *cmd, char **envp) {
-    pid_t pid;
-    int fd[2];
-    if (pipe(fd) < 0) {
-        handle_pipe_error();
-    }
-    pid = fork();
-    if (pid < 0) {
-        handle_fork_error();
-    }
-    if (pid == 0) {
-        child_process_logic(fd, cmd, envp);
-    } else {
-        close(fd[1]);
-        if (dup2(fd[0], STDIN_FILENO) < 0) {
-            perror("Error duplicating file descriptor for pipe");
-            exit(EXIT_FAILURE);
-        }
-        close(fd[0]);
-        wait(NULL);
-    }
+void	ft_pipex(char **argv, char **envp)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (pipe(fd) < 0)
+		handle_pipe_error();
+	pid = fork();
+	if (pid < 0)
+		handle_fork_error();
+	if (pid == 0)
+		child_process_logic(fd, *argv, envp);
+	else
+	{
+		close(fd[1]);
+		if (dup2(fd[0], STDIN_FILENO) < 0)
+		{
+			perror("Error duplicating file descriptor for pipe");
+			exit(EXIT_FAILURE);
+		}
+		close(fd[0]);
+		wait(NULL);
+	}
 }
