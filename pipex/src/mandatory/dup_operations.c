6ac57bbd92a570826886d47494556619d75146f7
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-14 19:23:27 +0200

    "Updated dup_and_close function formatting and timestamp in comment"

--- pipex/src/mandatory/dup_operations.c
+++ pipex/src/mandatory/dup_operations.c
@@ -6,15 +6,15 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:16:27 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 17:52:54 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:23:20 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
-void dup_and_close(int oldfd, int newfd) {
-    if (dup2(oldfd, newfd) < 0) {
-        handle_error("Error duplicating file descriptor");
-    }
-    close(oldfd);
+void	dup_and_close(int oldfd, int newfd)
+{
+	if (dup2(oldfd, newfd) < 0)
+		handle_error("Error duplicating file descriptor");
+	close(oldfd);
 }
