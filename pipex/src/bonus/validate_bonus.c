c1c3bde6668b80c03f3c8a9f28693798e4979999
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 16:50:16 +0200

    "Updated validate_bonus.c: added comments, modified updated date, and reformatted code"

--- pipex/src/bonus/validate_bonus.c
+++ pipex/src/bonus/validate_bonus.c
@@ -6,12 +6,15 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:13:51 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 19:43:06 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:50:10 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * Validates the number of arguments passed to the program.
+ */
 void	validate_arguments(int argc)
 {
 	if (argc < 5)
@@ -21,6 +24,9 @@ void	validate_arguments(int argc)
 	}
 }
 
+/**
+ * Handles the forking and processing of commands.
+ */
 void	handle_fork_and_process(int argc, char **argv, char **envp, int i)
 {
 	pid_t	pid;
