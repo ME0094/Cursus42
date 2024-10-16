eeb8c06452377d56ed3fdd18eb4d5aa5cee07d92
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 18:08:47 +0200

    "Updated util_command.c: added function comments and modified update date"

--- pipex/src/mandatory/util_command.c
+++ pipex/src/mandatory/util_command.c
@@ -6,12 +6,17 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:20:39 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 19:39:56 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:08:44 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * This function splits the given command argument into an array of strings,
+ * attempts to find the command's path using the provided environment variables,
+ * and handles errors if the command path is not found.
+ */
 void	check_command(char *arg, char **envp)
 {
 	char	*path;
@@ -34,6 +39,11 @@ void	check_command(char *arg, char **envp)
 	}
 }
 
+/**
+ * This function iterates over the command-line arguments starting from the
+ * specified index and checks the validity of each command using the provided
+ * environment variables.
+ */
 void	ft_check_cmd(int argc, char **argv, char **envp, int i)
 {
 	while (i <= (argc - 2))
