52d4f25e8e999bbb9d981b1581e42ed747e2bca9
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 16:47:24 +0200

    "Updated main_bonus.c: added comment block and updated last update date"

--- pipex/src/bonus/main_bonus.c
+++ pipex/src/bonus/main_bonus.c
@@ -6,12 +6,18 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:14:08 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 18:34:22 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:47:16 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * This file contains the main function which serves as the entry point for the
+ * bonus part of the pipex project. It validates the arguments, checks the input,
+ * verifies the commands, duplicates file descriptors, and handles the forking
+ * and processing of commands.
+ */
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
