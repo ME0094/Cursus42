2b38bb168e0a91e561a5e744ac331f35622da8ea
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 16:54:47 +0200

    Added function comments for ft_free and open_file, and updated the last updated date in the file header.

--- pipex/src/mandatory/file_operations.c
+++ pipex/src/mandatory/file_operations.c
@@ -6,12 +6,15 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 17:28:09 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 19:29:20 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:54:35 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * Frees a dynamically allocated array of strings.
+ */
 void	ft_free(char **ptr)
 {
 	int	i;
@@ -22,6 +25,9 @@ void	ft_free(char **ptr)
 	free(ptr);
 }
 
+/**
+ * Opens a file with the specified flags and handles errors.
+ */
 int	open_file(char *file, int flags, const char *error_msg)
 {
 	int	fd;
@@ -35,6 +41,9 @@ int	open_file(char *file, int flags, const char *error_msg)
 	return (fd);
 }
 
+/**
+ * Opens a file with the specified mode and returns the file descriptor.
+ */
 int	ft_get_fd(char *file, int mode)
 {
 	int	fd;
