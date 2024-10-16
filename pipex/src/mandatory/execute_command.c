e3d72b42decae98244db0cbdb78fb6c22eb47279
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 16:53:38 +0200

    "Added function comment to execute_command and updated timestamp in file header"

--- pipex/src/mandatory/execute_command.c
+++ pipex/src/mandatory/execute_command.c
@@ -6,12 +6,18 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:16:42 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 19:24:57 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:53:28 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * This function attempts to execute a command specified by the given path
+ * with the provided arguments and environment variables. If the execution
+ * fails, it prints an error message, frees allocated memory, and exits the
+ * program with a failure status.
+ */
 void	execute_command(char *path, char **argv_sp, char **envp)
 {
 	if (execve(path, argv_sp, envp) == -1)
