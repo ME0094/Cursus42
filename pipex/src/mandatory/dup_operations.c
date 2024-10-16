1b3206519e645bca3fe99b2dbd3c49889e53227b
Martín Eliseo <martineliseo.info@gmail.com>
2024-10-16 16:51:50 +0200

    "Updated dup_operations.c: added function comment and updated timestamp"

--- pipex/src/mandatory/dup_operations.c
+++ pipex/src/mandatory/dup_operations.c
@@ -6,12 +6,15 @@
 /*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:16:27 by martirod          #+#    #+#             */
-/*   Updated: 2024/10/14 19:23:20 by martirod         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:51:40 by martirod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex.h"
 
+/**
+ * Duplicates a file descriptor and closes the old one.
+ */
 void	dup_and_close(int oldfd, int newfd)
 {
 	if (dup2(oldfd, newfd) < 0)
