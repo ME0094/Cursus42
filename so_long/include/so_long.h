ea349aeb8343cb4406c5aa63fd8e03f492931e38
Martin Eliseo <martineliseo.info@gmail.com>
2024-12-08 14:01:35 +0100

    Actualizar encabezados y agregar configuración de tareas para la compilación con gcc

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -1,40 +1,8 @@
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
-#include "../MLX42/include/MLX42/MLX42.h"
-
-// Define constants
-#define WIDTH 800
-#define HEIGHT 600
-#define TITLE "So Long"
-
-// Define enum for game states
-typedef enum {
-    GAME_START,
-    GAME_PLAYING,
-    GAME_PAUSED,
-    GAME_OVER
-} game_state;
-
-// Define struct for game data
-typedef struct {
-    void *mlx;
-    void *window;
-    int width;
-    int height;
-    int **map;
-    int player_x;
-    int player_y;
-    int collectibles;
-    int exits;
-    game_state state;
-} game_data;
-
-// Function declarations
-void init_game(game_data *game);
-void render_game(game_data *game);
-void handle_key_press(int key, game_data *game);
-void handle_key_release(int key, game_data *game);
-void update_game(game_data *game);
+# include "../lib/libft/libft.h"
+# include "../lib/libft/ft_printf/ft_printf.h"
+# include "../lib/MLX42/include/MLX42/MLX42.h"
 
 #endif
---
+++ so_long/src/main.c
@@ -0,0 +1,1 @@
+#include "../include/so_long.h"
