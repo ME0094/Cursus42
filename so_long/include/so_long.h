91d21e510b7bbfeb7bc88e68ff82c6e65df229e5
Martín Eliseo <martineliseo.info@gmail.com>
2024-12-30 15:53:34 +0100

    Agregar archivos fuente y encabezado al proyecto so_long

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martirod <martirod@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 15:52:42 by martirod          #+#    #+#             */
+/*   Updated: 2024/12/30 15:52:49 by martirod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
 # include "../lib/libft/libft.h"
 # include "../lib/libft/ft_printf/ft_printf.h"
